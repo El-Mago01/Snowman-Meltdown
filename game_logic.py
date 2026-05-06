643cecf8ee44a135d4d7a1e135d561be5c1266f6
El-Mago01 <Martin.Gorren@gmail.com>
2026-05-06 19:28:52 +0200

    Updated game_logic, removed debug info, added printed the wrongly guessed characters

--- game_logic.py
+++ game_logic.py
@@ -8,24 +8,33 @@ def get_random_word() -> str:
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
-def display_dashes_and_chars(secret_word, correct_guessed_chars) -> bool:
+def display_dashes_and_chars(secret_word, guessed_chars) -> bool:
     """
     Displays dashes and character guesses.
     Returns True if the secret word is guessed, False otherwise.
     """
     word_guessed = True
     for char in secret_word:
-        if char in correct_guessed_chars:
-            print(char, end=" ")
+        if char in guessed_chars.keys():
+            if guessed_chars[char] == True:
+                print(char, end=" ")
+            else:
+                print("_", end=" ")
+                word_guessed = False
         else:
             print("_", end=" ")
             word_guessed = False
+    print("\nYou already guessed the following characters wrong:")
+    for char in guessed_chars.keys():
+        if guessed_chars[char] == False:
+            print(char, end=" ")
+    print("")
     return word_guessed
 
-def display_game_state(mistakes: int,secret_word: str, correct_guessed_letters: list) -> bool:
+def display_game_state(mistakes: int,secret_word: str, guessed_letters: dict) -> bool:
     """Displays game state after a new guess"""
     print(aa.STAGES[mistakes])
-    return display_dashes_and_chars(secret_word, correct_guessed_letters)
+    return display_dashes_and_chars(secret_word, guessed_letters)
 
 def play_game():
     """ Contains the game logic:
@@ -50,22 +59,29 @@ def play_game():
     print("Welcome to Snowman Meltdown!")
     # print("Secret word selected: " + secret_word)  # for testing, later remove this line
     mistakes=0
-    correct_guessed_chars=[]
-    display_game_state(mistakes, secret_word, correct_guessed_chars)
+    guessed_chars={}
+    display_game_state(mistakes, secret_word, guessed_chars)
     game_won=False
     game_iteration=0
     while game_iteration < len(aa.STAGES)-1:
         guess = get_character()
         print("You guessed:", guess)
         # print(game_iteration)
-        if guess in secret_word and guess not in correct_guessed_chars:
-            correct_guessed_chars.append(guess)
+        if guess in secret_word and guess not in guessed_chars.keys():
+            guessed_chars[guess] = True
             # print(game_iteration)
-        elif guess not in secret_word:
+        elif guess not in secret_word and guess not in guessed_chars.keys():
+            mistakes += 1
+            guessed_chars[guess] = False
+            game_iteration += 1
+        elif guess not in secret_word and guess in guessed_chars.keys():
+            print("You already guessed the letter, sorry but this counts as an extra mistake")
             mistakes += 1
             game_iteration += 1
+        else:
+            print("You already correctly guessed this letter, no consequences!")
         print(game_iteration)
-        game_won=display_game_state(mistakes,secret_word,correct_guessed_chars)
+        game_won=display_game_state(mistakes,secret_word,guessed_chars)
         if game_won:
             break
     if game_won:
